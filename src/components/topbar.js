e77bbe4d70a554de8b376e15d09ebe9c96096f8b
Ene AL <kaixo.eneal@gmail.com>
2023-10-25 16:44:37 +0200

    topbar

---
+++ src/components/topbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function TopBar() {
+  return (
+    <div className="TopBar">
+      <div className="Left">
+        <h2>News Page</h2>
+      </div>
+      <div className="Right">
+        <div className="WeatherWidget">
+          {/* Aquí irá el widget del tiempo */}
+          WIDGET
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TopBar;
